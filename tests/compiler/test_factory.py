580b0c5352bf6bfd212c6c0c9b1bcbb19bd27923
helmbergerfabian <helmbergerfabian@gmail.com>
2025-06-24 18:47:42 +0200

    test kernel decorator

--- tests/compiler/test_factory.py
+++ tests/compiler/test_factory.py
@@ -1,9 +1,19 @@
 from pytest import MonkeyPatch
-from complex_langevin.compiler.factory import get_backend
-from complex_langevin.compiler.python_backend import PythonBackend
 
 
 def test_python_backend_selection(monkeypatch: MonkeyPatch):
     monkeypatch.setenv("MY_NUMBA_TARGET", "python")
+
+    from complex_langevin.compiler.factory import get_backend
+    from complex_langevin.compiler.python_backend import PythonBackend
+
     backend = get_backend()
     assert isinstance(backend, PythonBackend)
+
+def test_python_kernel_decotrator(monkeypatch: MonkeyPatch):
+    monkeypatch.setenv("MY_NUMBA_TARGET", "python")
+
+    from complex_langevin.compiler.factory import get_backend
+
+    backend = get_backend()
+    assert callable(backend.kernel)
