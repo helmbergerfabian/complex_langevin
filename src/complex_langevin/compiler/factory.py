827e34a694122f12449813584e1a2b53aec90c35
helmbergerfabian <helmbergerfabian@gmail.com>
2025-07-02 21:20:54 +0200

    numba target

--- src/complex_langevin/compiler/factory.py
+++ src/complex_langevin/compiler/factory.py
@@ -6,11 +6,14 @@ def get_backend():
         SimulationBackend: An instance of the backend class.'''
 
     import os
-    target = os.environ.get('MY_NUMBA_TARGET', 'numba').lower()
+    target = os.environ.get('MY_NUMBA_TARGET', 'python').lower()
     
     if target == 'python':
         from .python_backend import PythonBackend
         return PythonBackend()
-
+    
+    if target == 'numba':
+        from .numba_backend import NumbaBackend
+        return NumbaBackend()
     else:
         raise ValueError(f"Unknown target backend: {target}")
