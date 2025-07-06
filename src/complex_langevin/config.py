7885f882eaac27a6b62b30cc26151fc63e44d246
helmbergerfabian <helmbergerfabian@gmail.com>
2025-07-06 23:41:29 +0200

    int type

--- src/complex_langevin/config.py
+++ src/complex_langevin/config.py
@@ -7,6 +7,8 @@ CL_PRECISION = os.getenv("CL_PRECISION", "single").lower()
 if CL_PRECISION == "double":
     CL_REAL = np.float64
     CL_COMPLEX = np.complex128
+    CL_INT = np.int64
 else:
     CL_REAL = np.float32
     CL_COMPLEX = np.complex64
+    CL_INT = np.int32
