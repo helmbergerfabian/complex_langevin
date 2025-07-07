dc9878e01c7ba3c4951a7d4bffc63ebe9a4ef2c6
helmbergerfabian <helmbergerfabian@gmail.com>
2025-07-07 14:13:46 +0200

    remove use_cuda

--- src/complex_langevin/simulation/state.py
+++ src/complex_langevin/simulation/state.py
@@ -15,13 +15,13 @@ class SimState:
     """
     def __init__(self, n_seeds: int):
         self.n_seeds = n_seeds
-        # self.use_cuda = (target == "cuda")
-
+        
         self.phi_read = np.zeros(n_seeds, dtype=CL_COMPLEX)
         self.phi_write = self.phi_read.copy()
 
         self.noise = np.zeros(n_seeds, dtype=CL_REAL)
         self.langevin_time = np.zeros(n_seeds, dtype=CL_REAL)
 
+        
     def swap_buffers(self):
         self.phi_read, self.phi_write = self.phi_write, self.phi_read
