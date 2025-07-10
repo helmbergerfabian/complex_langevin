0f2b1a471470c0cd19a63082e401b49647a6a57e
= <=>
2025-07-10 18:49:47 +0200

    dtadaKernel type

--- src/complex_langevin/utils/cl_types.py
+++ src/complex_langevin/utils/cl_types.py
@@ -1,5 +1,6 @@
-# complex_langevin/models/cl_types.py
+# complex_langevin/utils/cl_types.py
 from typing import Protocol, runtime_checkable, Any
+from complex_langevin.config import CL_REAL, CL_COMPLEX
 
 @runtime_checkable
 class DriftKernel(Protocol):
@@ -13,5 +14,10 @@ class NoiseKernel(Protocol):
 
 @runtime_checkable
 class EvolveKernel(Protocol):
-    def __call__(self, idx: int, phi_arr: Any, drift_arr: Any, noise_arr: Any, dt_arr: Any) -> None: 
+    def __call__(self, idx: int, phi_arr: Any, drift_arr: Any, noise_arr: Any, dt_ada_arr: Any, dt_base: CL_REAL) -> None: 
+        ...
+
+@runtime_checkable
+class dtadaKernel(Protocol):
+    def __call__(self, idx: int, dt_ada_arr: Any, drift_arr: Any) -> None: 
         ...
