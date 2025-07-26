1aca13e4b7a7ddb36e0f6ffd108343a0efd0c903
= <=>
2025-07-26 18:00:09 +0200

    logger init

--- src/complex_langevin/config.py
+++ src/complex_langevin/config.py
@@ -1,14 +1,23 @@
 ### src/complex_langevin/config.py
-import numpy as np
+from numpy import float32, float64, complex64, complex128, int64, int32
 import os
 
 CL_PRECISION = os.getenv("CL_PRECISION", "single").lower()
 
 if CL_PRECISION == "double":
-    CL_REAL = np.float64
-    CL_COMPLEX = np.complex128
-    CL_INT = np.int64
+    CL_REAL = float64
+    CL_COMPLEX = complex128
+    CL_INT = int64
 else:
-    CL_REAL = np.float32
-    CL_COMPLEX = np.complex64
-    CL_INT = np.int32
+    CL_REAL = float32
+    CL_COMPLEX = complex64
+    CL_INT = int32
+
+
+from time import time
+import os
+if os.getenv("VERBOSE", "False") == "True":
+    def log(self, sender, message):
+        print(f"{hex(id(self))}\t[{sender}]\t{message}\t({time()-float(os.environ.get('RUNNER_START_TIME', 0))})")
+else:
+    def log(self, sender, message): pass
