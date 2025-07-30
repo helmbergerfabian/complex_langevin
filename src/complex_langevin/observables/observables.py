45b9209d91f6909939e99e48bb29e8dcb4f632ff
helmbergerfabian <helmbergerfabian@gmail.com>
2025-07-30 15:24:10 +0200

    remove count[i] > 0 in pack_bloc_means

--- src/complex_langevin/observables/observables.py
+++ src/complex_langevin/observables/observables.py
@@ -29,7 +29,7 @@ def pack_block_means(obs_sum, count, tau, tau_last_flush, flush_mask, block_mean
     Based on a flush_mask, block_means are calculated and running sum is reset. 
     """
     for i in prange(obs_sum.shape[0]):
-        if flush_mask[i] and count[i] > 0:
+        if flush_mask[i]:
             block_means[i] = obs_sum[i] / count[i]
             obs_sum[i] = 0.0
             count[i] = 0
@@ -51,8 +51,12 @@ class MomentObservable():
         self.obs_running_sum = zeros(shape = state.n_seeds, dtype=CL_COMPLEX)
         self.block_means = np.zeros_like(self.obs_running_sum)
         self.count = zeros(shape = state.n_seeds, dtype=CL_INT)
+
         self.flush_mask = zeros(shape = state.n_seeds, dtype=bool)
         self.last_flush = zeros(shape = state.n_seeds, dtype=CL_REAL)
+        self.last_meas = zeros(shape = state.n_seeds, dtype=CL_REAL)
+        self.cold = np.full(shape = state.n_seeds, fill_value=True)
+
         # self.flush_times = [[] for _ in range(state.n_seeds)]
         # self.hist = [[] for _ in range(state.n_seeds)]
         self.delta_tau_block = delta_tau_block or 1
@@ -94,15 +98,23 @@ class MomentObservable():
        
     def generate_kernel(self):
         @self.backend.kernel
-        def _moment_kernel(idx, phi_arr, obs_running_sum, count, order):
+        def _moment_kernel(idx, phi_arr, obs_running_sum, count, order, langevin_time, last_meas):
             phi_idx = phi_arr[idx]
             obs = phi_idx**order
             obs_running_sum[idx] += obs
             count[idx] += 1
+            last_meas[idx] = langevin_time[idx]
         return _moment_kernel
     
     def observe(self):
-        self.backend.parallel_loop(self.kernel, self.state.n_seeds, self.state.phi_read, self.obs_running_sum, self.count, self.order)
+        # self.backend.parallel_loop(self.kernel, self.state.n_seeds, self.state.phi_read, self.obs_running_sum, self.count, self.order)
+        self.cold = (self.state.langevin_time - self.last_meas) > self.state.dt_base/10
+        # self.log(f"lt: {self.state.langevin_time}")
+        # self.log(f"last meas: {self.last_meas}")
+        # self.log(f"cold:{self.cold}")
+
+        self.backend.act_parallel_loop(self.kernel, self.cold, self.state.n_seeds, self.state.phi_read, self.obs_running_sum, self.count, self.order, self.state.langevin_time, self.last_meas)
+        
         # self.log(f"Field: {self.state.phi_read}")
         # self.log("Kernel executed: ")
         # self.log(f"Langevin times: {self.state.langevin_time}")
@@ -115,7 +127,7 @@ class MomentObservable():
                 self.flush_mask, self.daq.block_counts, self.delta_tau_block,
                 self.num_blocks
             )
-            # self.log(f"New Flush Mask: {self.flush_mask}")
+            self.log(f"New Flush Mask: {self.flush_mask}")
 
             if np.any(self.flush_mask):  # only flush if needed
                 pack_block_means(
@@ -127,7 +139,7 @@ class MomentObservable():
                 valid_idx = np.where(~np.isnan(self.block_means))[0]
                 valid_vals = self.block_means[valid_idx]
                 if valid_idx.shape[0] > 0:
-                    # self.log(f"flusing seed {valid_idx}")
+                    self.log(f"flusing seed {valid_idx}")
                     self.out_q.put((valid_idx, valid_vals))
 
     def reset(self):
